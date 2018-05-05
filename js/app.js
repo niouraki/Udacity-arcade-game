cc067a1ecbc6936dec9a74a09c91a0a3281d7385
Charikleia Niouraki <niouraki@gmail.com>
2018-05-05 23:10:09 +0300

    Create collide code and add scores

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+var counting = 0;
+
+//Create the score element
+var score = document.createElement("div");
+score.innerHTML = "Score: 0";
+document.body.appendChild(score);
+
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
@@ -6,80 +13,103 @@ var Enemy = function(x, y, speed) {
     this.speed = speed;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     var distance = this.speed * dt;
     this.x = this.x + distance;
 
-//Make the enemies reappear after exit the screen on the right
+//Make the enemies reappear after they exit the screen on the right
     if (this.x > 500) {
       this.x = 0;
     }
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// This class requires an update(), render() and
-// a handleInput() method.
+
 var Player = function (x, y) {
   this.sprite = 'images/char-boy.png';
   this.x = x;
   this.y = y;
 };
 
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
+//Check collision
+Player.prototype.update = function() {
+  for(var i = 0; i < allEnemies.length; i++) {
+    if(allEnemies[i].x <= this.x + 40 &&
+        allEnemies[i].x >= this.x - 40 &&
+        allEnemies[i].y <= this.y + 40 &&
+        allEnemies[i].y >= this.y - 40) {
+          this.reset();
+          this.removeScore();
+      }
+    }
 };
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//Make player move but don't allow it to move off screen
 Player.prototype.handleInput = function(direction) {
     if (direction === "right" && this.x < 400) {
       this.x += 100;
     }else if (direction === "left" && this.x > 0) {
       this.x -= 100;
     }else if (direction === "down" && this.y < 400) {
-      this.y += 100;
+      this.y += 90;
     }else if (direction === "up" && this.y > 0) {
-      this.y -= 100;
-    }else if (direction === "up" && this.y === 0) {
+      this.y -= 90;
+    }else if (direction === "up" && this.y === -50) {
       this.reset();
+      this.countScore();
     }
-    console.log(this.x, this.y);
 };
 
-//Resets the position of the player once he or she reached the water and tries to cross it
+//Reset the position of the player once it reaches the water and try to cross it
 Player.prototype.reset = function() {
     this.x = 200;
     this.y = 400;
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+//When player reach water add one point to score
+Player.prototype.countScore = function() {
+    counting++
+    score.innerHTML = "Score: " + counting;
+
+    if (counting === 5) {
+      alert("Great job! Keep going.")
+    }
+}
+
+//When player collides remove one point from score
+Player.prototype.removeScore = function() {
+  counting--
+  score.innerHTML = "Score: " + counting;
+
+  if(counting === -1) {
+    alert("Stay focused. You can do it!")
+  }
+}
 
 //instantiates the player object
 var player = new Player(200, 400);
 
-//creates an array, instantiates the enemy object and pushes it inside the array
-var enemy1 = new Enemy(8, 225, 120);
-var enemy2 = new Enemy(8,145, 80);
-var enemy3 = new Enemy(8, 62, 90);
-var allEnemies = [enemy1, enemy2, enemy3];
+//create an array, instantiates the enemy object and push it inside the array
+var enemy1 = new Enemy(40, 225, 100);
+var enemy2 = new Enemy(40,145, 75);
+var enemy3 = new Enemy(40, 62, 90);
+var enemy4 = new Enemy(0, 225, 60);
+var enemy5 = new Enemy(0, 145, 140);
+var enemy6 = new Enemy(0, 62, 150);
+var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
 
 
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
